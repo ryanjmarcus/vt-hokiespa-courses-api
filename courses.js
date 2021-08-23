6b63434a27c1727255fd3028082e8d61d122a0c6
Ryan Marcus <ryanjmarcus@gmail.com>
2021-08-22 20:33:21 -0400

    Fixed missing courses. Updated logging.

--- courses.js
+++ courses.js
@@ -1,34 +1,33 @@
-var count = 0;
 var offset = 0;
 var maxSize = 500;
 
-var someCourses;
+var someCourses = [];
 var allCourses = [];
 
 // Semester Codes: [Fall = 09, Winter = 12, Spring = 01, Summer = 06]
 // Examples: Fall 2021 = "202109", Summer 2019 = "201906"
 // Year + Semester Code
-var term = "202101"
+var term = "202109"
 
 // Total courses for the given semester
 // Perform an empty search and look for "Records: #" in the bottom right
-var totalCourses = 10645;
+var totalCourses = 10590;
 
-
-while (offset <= totalCourses - maxSize) {
-
+while (totalCourses - offset > 0) {
     var url = 'https://apps.es.vt.edu/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_term=' + term + '&startDatepicker=&endDatepicker=&uniqueSessionId=sa9jy1591815980896&pageOffset=' + offset +'&pageMaxSize=' + maxSize + '&sortColumn=subjectDescription&sortDirection=asc';
     
     await fetch(url)
         .then(res => res.json())
         .then(data => someCourses = data.data)
 
-    console.log(someCourses);
-
     offset += 500;
-    count += 1;
-    console.log(count);
-
     allCourses = allCourses.concat(someCourses);
+
+    console.log(someCourses);
+    if (totalCourses - offset > 0) {
+        console.log(offset.toLocaleString() + " courses fetched | " + (totalCourses - offset).toLocaleString() + " courses left."); 
+    } else {
+        console.log("Done! Successfully fetched: " + totalCourses.toLocaleString() + " courses. Click copy below.")
+    }   
 }
 console.log(JSON.stringify(allCourses))
